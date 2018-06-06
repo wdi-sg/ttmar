d3b24bd2fa5f2066d75d8f32b34bb5a33b525c00
Julian Poh <julianpoh01@gmail.com>
2018-06-06 22:48:35 +0800

    completed bonus 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
 
     //Set colours to use as background
-    var colors = ["red", "pink", "orange", "brown", "grey", "yellow", "green", "cyan", "violet", "tomato"];
+    var colors = ["red", "pink", "orange", "brown", "grey", "green", "cyan", "violet", "tomato"];
 
     //Function to randomize selected colors
     var randomColors = function() {
@@ -10,11 +10,39 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
     var firstLink = document.getElementById('first_paragraph');
-    var firstParagraph = document.getElementsByClassName('first_paragraph');
-    
+    var firstParagraph = document.querySelector('.first_paragraph');
+    var secondLink = document.getElementById('second_paragraph');
+    var secondParagraph = document.querySelector('.second_paragraph');
+    var thirdLink = document.getElementById('first_chorus');
+    var firstChorus = document.querySelector('.first_chorus');
+    var fourthLink = document.getElementById('third_paragraph');
+    var thirdParagraph = document.querySelector('.third_paragraph');
+    var fifthLink = document.getElementById('fourth_paragraph');
+    var fourthParagraph = document.querySelector('.fourth_paragraph');
+
+
     firstLink.addEventListener('click', function(){
-        this.style.backgroundColor = randomColors();
+        firstParagraph.style.backgroundColor = randomColors();
+    });
+    
+    secondLink.addEventListener('click', function(){
+        secondParagraph.style.display = "block";
+        secondParagraph.style.backgroundColor = randomColors();
     });
 
+    thirdLink.addEventListener('click', function(){
+        firstChorus.style.display = "block";
+        firstChorus.style.backgroundColor = randomColors();
+    });
+
+    fourthLink.addEventListener('click', function(){
+        thirdParagraph.style.display = "block";
+        thirdParagraph.style.backgroundColor = randomColors();
+    });    
+
+    fifthLink.addEventListener('click', function(){
+        fourthParagraph.style.display = "block";
+        fourthParagraph.style.backgroundColor = randomColors();
+    });
 
 })
